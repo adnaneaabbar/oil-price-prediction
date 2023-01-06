af726da3fb573ca57bc7c63d346d0d40f1e950fe
najwa errachidy <najwa.errachidy@gmail.com>
2023-01-06 23:06:40 +0100

    feature scaling and reshaping x,y train/test

--- model_training/main.py
+++ model_training/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv("raw_data.csv", sep=",")
 
@@ -21,9 +22,8 @@ df = df[df["DCOILBRENTEU"] != "."]
 # plt.show()
 
 batch_size = 64
-timesteps = 30
+timesteps = 10
 test_percent = 0.1
-timesteps = 30
 
 # getting the training size
 def get_train_size(data, batch_size, test_percent):
@@ -46,18 +46,51 @@ def get_test_size(data, batch_size):
             val.append(i)
     return max(val)
 
-print(len(df) * (1 - test_percent))                         # 8126.1
-print(get_train_size(df, batch_size, test_percent))         # 8064
+# print(len(df) * (1 - test_percent))                         # 8126.1
+# print(get_train_size(df, batch_size, test_percent))         # 8064
 
 train_size = get_train_size(df, batch_size, test_percent) + 2 * timesteps
 df_train = df[0: train_size]  # 7892, 2
 training_set = df_train.iloc[:, 1:2].values
-print(training_set.shape)                                   # (8124, 1)
+# print(training_set.shape)                                   # (8124, 1)
 
-print(len(df) * (test_percent))                             # 902.9
-print(get_test_size(df, batch_size))                        # 8956
+# print(len(df) * (test_percent))                             # 902.9
+# print(get_test_size(df, batch_size))                        # 8956
 
 test_size = get_test_size(df, batch_size) + 2 * timesteps
 df_test = df[train_size: test_size]                         
 test_set = df_test.iloc[:, 1:2].values
-print(test_set.shape)                                       # (892, 1)
+# print(test_set.shape)                                       # (892, 1)
+
+# Feature Scaling
+scaler = MinMaxScaler(feature_range=(0, 1))
+train_scaled = scaler.fit_transform(training_set)
+test_scaled = scaler.fit_transform(test_set)
+print(train_scaled.shape)                                  # (8084, 1)
+print(test_scaled.shape)                                   # (916, 1)
+
+# Constructing X, Y train
+x_train = []
+y_train = []
+for i in range(timesteps, train_size - timesteps):
+    x_train.append(train_scaled[i - timesteps:i, 0])
+    y_train.append(train_scaled[i:i + timesteps, 0])
+
+x_test = []
+for i in range(timesteps, (test_size - train_size) - timesteps):
+    x_test.append(test_scaled[i - timesteps:i, 0])
+
+x_train = np.asarray(x_train).astype(np.float32)
+y_train = np.asarray(y_train).astype(np.float32)
+x_test = np.asarray(x_test).astype(np.float32)
+print(x_train.shape)  # (8064, 10)
+print(y_train.shape)  # (8064, 10)
+print(x_test.shape)  # (896, 10)
+
+# Reshaping X, Y train
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+y_train = np.reshape(y_train, (y_train.shape[0], y_train.shape[1], 1))
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+print(x_train.shape)  # (8064, 10, 1)
+print(y_train.shape)  # (8064, 10, 1)
+print(x_test.shape)  # (896, 10, 1)
