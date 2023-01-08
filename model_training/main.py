c1205f0c8e9bf125b17192142cf539abb6702499
Mariia KLIMINA <mklimina@e1r6p9.clusters.42paris.fr>
2023-01-08 20:28:44 +0100

    timesteps = 30, model run

--- model_training/main.py
+++ model_training/main.py
@@ -160,4 +160,4 @@ rmse = math.sqrt(mean_squared_error(test_set[timesteps:len(y_test)], y_test[0:le
 print("RMSE : ", rmse)
 
 mae = mean_absolute_error(test_set[timesteps:len(y_test)], y_test[0:len(y_test) - timesteps]) #4.094
-print("MAE : ", mae.numpy()) #2.71
+print("MAE : ", mae.numpy()) #2.6807
